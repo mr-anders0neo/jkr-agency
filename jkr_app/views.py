e46d934e6db37146e9d97db5d9b85d3a4c2b3123
rupa <hema.rupasri@gmail.com>
2022-07-21 17:21:43 -0400

    [Rupa] UI Changes flask app

--- jkr_app/views.py
+++ jkr_app/views.py
@@ -3,17 +3,18 @@ from flask import render_template
 from . import app
 
 @app.route("/")
+@app.route("/index.html")
 def home():
     return render_template("index.html")
 
-@app.route("/about-us/")
+@app.route("/about-us.html")
 def about():
     return render_template("about-us.html")
 
-@app.route("/contact-us/")
+@app.route("/contact-us.html")
 def contact():
     return render_template("contact-us.html")
 
-@app.route("/services/")
+@app.route("/services.html")
 def services():
     return render_template("services.html")
